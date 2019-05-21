dc65d6488aabbf729dba2a030efcaefefd1a4437
adamGuinea <adamguinea22@gmail.com>
2019-05-21 13:46:12 +1200

    automating deployment and clearing cache

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -56,8 +56,8 @@ export default class Home extends Component {
   renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+        <h1>Latenotes</h1>
+        <p className="loader" />
       </div>
     );
   }
